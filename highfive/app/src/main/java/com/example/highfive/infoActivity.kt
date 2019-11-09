97e4845df6175e9b7ab634b7d9166735d5a639d4
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-11-09 18:43:10 +0530

    comments 1

---
+++ highfive/app/src/main/java/com/example/highfive/infoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.highfive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class infoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
